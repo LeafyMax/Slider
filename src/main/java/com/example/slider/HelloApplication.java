f10ee5733036fdd0a8eea100c1cb7cbda738f685
Max <mbla657@g.educaand.es>
2025-09-25 15:45:04 +0200

    version final

--- src/main/java/com/example/slider/HelloApplication.java
+++ src/main/java/com/example/slider/HelloApplication.java
@@ -2,11 +2,15 @@ package com.example.slider;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,7 +19,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         GridPane gridPane = new GridPane();
-        Scene scene = new Scene(gridPane, 300,300);
+        Scene scene = new Scene(gridPane, 400,500);
         gridPane.setAlignment(Pos.CENTER);
         gridPane.setVgap(10);
 
@@ -48,12 +52,37 @@ public class HelloApplication extends Application {
 
         Label label5 = new Label("Lo mismo pero se pega el puntero");
         Slider sl4 = new Slider(0,100,50);
+        sl4.setMinorTickCount(0);
         sl4.setShowTickMarks(true);
         sl4.setShowTickLabels(true);
         sl4.setSnapToTicks(true);
+        sl4.setBlockIncrement(25);
 
         gridPane.add(label5,0,4);
         gridPane.add(sl4,1,4);
+
+        Label label6 = new Label("Slider vertical ");
+        Slider sl5 = new Slider(0,100,50);
+        sl5.setOrientation(Orientation.VERTICAL);
+
+        gridPane.add(label6,0,5);
+        gridPane.add(sl5,1,5);
+
+        Label label7 = new Label("Ejemplo de uso: ");
+        Slider sl6 = new Slider(0,240,0);
+        Rectangle rect =  new Rectangle(100,100, Color.BLACK);
+
+        sl6.valueProperty().addListener((obs, oldVal, newVal) -> {int red = newVal.intValue();
+        rect.setFill(Color.rgb(red,0,red));
+        });
+
+
+
+
+        gridPane.add(label7,0,6);
+        gridPane.add(sl6,1,6);
+        gridPane.add(rect,0,7);
+
         stage.setTitle("Slider");
         stage.setScene(scene);
         stage.show();
